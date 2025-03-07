a3c63d17964b7bc09e86e8caf9fc4a67f4bb8f56
Kafi <nukmanalkafi15@gmail.com>
2025-03-07 14:44:44 +0700

    up

---
+++ muhamadnukmanalkafi_2211102223_modul3/android/app/src/main/kotlin/com/example/muhamadnukmanalkafi_2211102223_modul3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.muhamadnukmanalkafi_2211102223_modul3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ muhamadnukmanalkafi_2211102223_modul3/lib/for2.dart
@@ -0,0 +1,8 @@
+void main() {
+  var mahasiswa = ['budi', 'bull', 'Kusuma', 'icad', 'ario']; 
+
+  mahasiswa.forEach((nama) { 
+    print('Nama Mahasiswa: $nama'); 
+  });
+}
+
